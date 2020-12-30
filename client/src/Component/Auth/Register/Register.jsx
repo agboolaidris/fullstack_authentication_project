de5b6cd4093c2c918e9b4fcb3c0dd487ebc6e358
Agboolaidris <agboolaisholaidreez@gmail.com>
2020-12-30 19:14:28 +0100

    update files

--- client/src/Component/Auth/Register/Register.jsx
+++ client/src/Component/Auth/Register/Register.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Register() {
-  return <div className="Register">Register</div>;
+  return (
+    <div className="register">
+      <div className="col-1">col-md-4</div>
+      <div className="col-2">col-md-8</div>
+    </div>
+  );
 }
 
 export default Register;
