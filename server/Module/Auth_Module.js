c32fb9b830db055a725dba484d58a7361402121d
Agboolaidris <agboolaisholaidreez@gmail.com>
2020-12-30 07:31:15 +0100

    update files

---
+++ server/Module/Auth_Module.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+const schema = mongoose.Schema
+
+
+new_schema = new schema({
+    username:{unique:true, minlength:6, maxlength:10, required:true, type:String},
+    email:{unique:true, required:true, type:String},
+    password:{ minlength:6, maxlength:10, required:true, type:String}
+})
+
+const User =mongoose.model('User',new_schema)
+
+module.exports = User
