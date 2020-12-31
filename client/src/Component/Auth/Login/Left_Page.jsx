6ac11a5e6398aa05dd3a9884f7beb99285e3ce81
Agboolaidris <agboolaisholaidreez@gmail.com>
2020-12-31 14:13:15 +0100

    update files

---
+++ client/src/Component/Auth/Login/Left_Page.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import {
+  faGooglePlusSquare,
+  faFacebookSquare,
+  faTwitterSquare,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faKey, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+function Left_Page() {
+  return (
+    <div className="div">
+      <h1>SIGN IN</h1>
+      <div className="icon">
+        <a>
+          <FontAwesomeIcon icon={faFacebookSquare} size="2x" />
+        </a>
+        <a>
+          <FontAwesomeIcon icon={faGooglePlusSquare} size="2x" />
+        </a>
+        <a>
+          <FontAwesomeIcon icon={faTwitterSquare} size="2x" />
+        </a>
+      </div>
+      <p> or use your email for registration</p>
+      <form action="">
+        <div className="input">
+          <label htmlFor="">
+            <FontAwesomeIcon icon={faEnvelope} />
+          </label>
+          <input type="password" placeholder="Email" />
+        </div>
+        <div className="input">
+          <label htmlFor="">
+            <FontAwesomeIcon icon={faKey} />
+          </label>
+          <input type="password" placeholder=" Password" />
+        </div>
+
+        <button>LOG IN</button>
+        <div className="login">
+          <span>
+            don't have an account <Link to="/register">Register</Link>
+          </span>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Left_Page;
--- client/src/Component/Auth/Login/Login.jsx
+++ client/src/Component/Auth/Login/Login.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Left_page from "./Left_Page";
+import Right_page from "./Right_page";
 
 function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="login">
+      <div className="col-1">
+        <Left_page />
+      </div>
+      <div className="col-2">
+        <Right_page />
+      </div>
+    </div>
+  );
 }
 
 export default Login;
