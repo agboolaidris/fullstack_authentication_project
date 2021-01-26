de8dd32ceb36ed228fdc2b18377e8c01b136173f
idris agboola <agboolaisholaidreez@gmail.com>
2021-01-26 01:02:35 +0100

    update files

--- server/Controller/Logout.js
+++ server/Controller/Logout.js
@@ -1,5 +1,16 @@
-const Logout = (req,res)=>{
-   res.json({msg:'tttt'})
+const User = require('../Module/AuthModule')
+const Logout = async(req,res)=>{
+  try{
+    const user = await User.findById(req.userID)
+    if(user){   
+         res.cookie()
+         res.json({msg:'logout successful'})
+      }
+  } 
+
+   catch(err){
+       res.status(400).json({msg:err.message})
+   }
 }
 
 module.exports = Logout
