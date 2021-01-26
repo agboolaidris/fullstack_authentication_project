31b1540bdc00311ad25a5e4faf0a92c6fbece006
idris agboola <agboolaisholaidreez@gmail.com>
2021-01-26 17:12:09 +0100

    update files

--- client/src/Component/Auth/Register/Register.jsx
+++ client/src/Component/Auth/Register/Register.jsx
@@ -2,44 +2,41 @@ import React from "react";
 import Facebook from "../Oauth/Facebook";
 import Google from "../Oauth/Google";
 import RegisterForm from "./registerForm";
-import {connect} from 'react-redux'
-import {oauthRegister} from '../../../Action/AuthAction'
+import { connect } from "react-redux";
+import { oauthRegister } from "../../../Action/AuthAction";
 
-
-
-function Register({isLoading, oauthRegister}) {
-  console.log(oauthRegister)
+function Register({ isLoading, oauthRegister }) {
+  console.log(oauthRegister);
   return (
     <>
-    <div className={isLoading ? 'isloading' : 'not-loading'}><span></span></div>
-    <div className="register">
-      <div className="register-form">
-        <div className="div">
-          <h1>Create Account</h1>
-          <RegisterForm />
-        </div>
+      <div className={isLoading ? "isloading" : "not-loading"}>
+        <span></span>
       </div>
-      <div className="social-register">
-        <div>
-          <p>Register with</p>
-          <div className="social">
-            <Facebook oauthRegister={oauthRegister} />
-            <Google oauthRegister={oauthRegister} />
-            
+      <div className="register">
+        <div className="register-form">
+          <div className="div">
+            <h1>Create Account</h1>
+            <RegisterForm />
+          </div>
+        </div>
+        <div className="register-social">
+          <div>
+            <p>Register with</p>
+            <div className="social">
+              <Facebook oauthRegister={oauthRegister} />
+              <Google oauthRegister={oauthRegister} />
+            </div>
           </div>
         </div>
       </div>
-    </div>
-
-     </>
+    </>
   );
 }
 
-const mapStateToProps = (state)=>{
-  
+const mapStateToProps = (state) => {
   return {
-    isLoading:state.Auth.isLoading
-  }
-}
+    isLoading: state.Auth.isLoading,
+  };
+};
 
-export default connect(mapStateToProps, {oauthRegister})(Register);
+export default connect(mapStateToProps, { oauthRegister })(Register);
