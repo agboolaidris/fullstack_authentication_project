23db8fec3f990406d1beb8dd4f3b8df90d88abe7
idris agboola <agboolaisholaidreez@gmail.com>
2021-01-27 07:11:19 +0100

    update files

--- client/src/Component/Auth/Forgetpassword/Forgetpassword.jsx
+++ client/src/Component/Auth/Forgetpassword/Forgetpassword.jsx
@@ -1,8 +1,15 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 function Forgetpassword() {
-  const handleSubmit = () => {};
+  const [state, setstate] = useState({
+    email: "",
+  });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(state);
+  };
   return (
     <div className="forget">
       <div className="forget-form">
@@ -12,21 +19,25 @@ function Forgetpassword() {
           will be sent to the email with a link to change your password as soon
           as you submit the correct email address.
         </p>
+        <form onSubmit={handleSubmit}>
+          <div className="input">
+            <label htmlFor="email">
+              <FontAwesomeIcon icon={faEnvelope} />
+            </label>
+            <input
+              type="email"
+              placeholder="Email"
+              onChange={(e) => setstate({ ...state, email: e.target.value })}
+              value={state.email}
+              id="email"
+            />
+          </div>
+          <div className="btn-container">
+            <Link to="/login">LOG IN</Link>
+            <button>SUBMIT</button>
+          </div>
+        </form>
       </div>
-      <form>
-        <div className="input">
-          <label htmlFor="email">
-            <FontAwesomeIcon icon={faEnvelope} />
-          </label>
-          <input
-            type="email"
-            placeholder="Email"
-            onChange={(e)=>}
-            value={state.email}
-            id="email"
-          />
-        </div>
-      </form>
     </div>
   );
 }
