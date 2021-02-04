f71c0c637fdbd035be94c809f30c796ee31f59cb
idris agboola <agboolaisholaidreez@gmail.com>
2021-02-04 14:12:49 +0100

    update files

--- client/src/Component/Auth/Logout.jsx
+++ client/src/Component/Auth/Logout.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHome, faBlog } from "@fortawesome/free-solid-svg-icons";
-
-function Logout() {
+import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { Logout } from "../../Action/AuthAction";
+import { connect } from "react-redux";
+function logout({ Logout }) {
   return (
-    <div className="logout">
+    <div className="logout" onClick={() => Logout()}>
       <FontAwesomeIcon icon={faHome} />
       <button>Logout</button>
     </div>
   );
 }
 
-export default Logout;
+export default connect(null, { Logout })(logout);
