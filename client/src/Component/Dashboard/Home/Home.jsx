cf4f9799ab1cf4078388c2a437483dac1251485b
idris agboola <agboolaisholaidreez@gmail.com>
2021-02-05 11:34:26 +0100

    update files

--- client/src/Component/Dashboard/Home/Home.jsx
+++ client/src/Component/Dashboard/Home/Home.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Dashboard() {
-  return <div className="dashboard">jjjjj</div>;
+  return (
+    <div className="dashboard">
+      <div className="div">
+        <div class="card">
+          <div className="img-container">
+            <img
+              src={`https://images.unsplash.com/photo-1612490020092-e4f7a3d17dec?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80`}
+            />
+          </div>
+          <div className="content">
+            <p class="title">Is Apple a Design Company?</p>
+            <p>
+              Apple is more than a tech company; it became a culture unto
+              itself, a passion of most of people and the birthplace of the
+              world’s most revolutionized products.{" "}
+              <Link class="button" to="/">
+                Read More
+              </Link>
+            </p>
+            <div>
+              <button>Delete</button>
+              <button>Like</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Dashboard;
--- client/src/Component/Dashboard/PostBlog/Post.jsx
+++ client/src/Component/Dashboard/PostBlog/Post.jsx
@@ -1,7 +1,47 @@
 import React from "react";
 
 function Post() {
-  return <div className="post">Post</div>;
+  const handleChange = () => {};
+  return (
+    <div className="post">
+      <form>
+        <div>
+          <label>Title</label>
+          <input type="text" required />
+        </div>
+        <div>
+          <label>Categories</label>
+          <select onChange={handleChange} required>
+            <option>------select------</option>
+            <option value="education">Education</option>
+            <option value="technology">Technology</option>
+            <option value="business">Business</option>
+            <option value="politics">Politics</option>
+            <option value="health">Health</option>
+            <option value="fashion">Fashion</option>
+            <option value="love">Love</option>
+            <option value="other">Other</option>
+          </select>
+        </div>
+        <div>
+          <label>image</label>
+          <input
+            onChange={handleChange}
+            type="file"
+            required
+            className="img-input"
+          />
+        </div>
+        <div>
+          <label>Body</label>
+          <textarea onChange={handleChange} required></textarea>
+        </div>
+        <div>
+          <button>SUBMIT</button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default Post;
