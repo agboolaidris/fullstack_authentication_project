97f0db00028c91f5f26c7764689dd8dd1b57aec7
idris agboola <agboolaisholaidreez@gmail.com>
2021-02-07 12:05:13 +0100

    update files

---
+++ client/src/Component/Dashboard/UserBlog/Card.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+function Card({ blog }) {
+  return (
+    <div className="card">
+      <div className="img-container">
+        <img src={blog.image} />
+      </div>
+      <div className="content">
+        <p className="title">{blog.title}</p>
+        <p className="p">
+          {blog.body}.{" "}
+          <Link className="button" to="/">
+            Read More
+          </Link>
+        </p>
+        <div>
+          <button className="delete">Delete</button>
+          <button>Like</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
