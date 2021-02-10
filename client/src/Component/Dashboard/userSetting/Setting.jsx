568ddcf56a98b7780149a351ccdf6f4179a4446f
idris agboola <agboolaisholaidreez@gmail.com>
2021-02-10 15:59:35 +0100

    update files

--- client/src/Component/Dashboard/userSetting/Setting.jsx
+++ client/src/Component/Dashboard/userSetting/Setting.jsx
@@ -1,11 +1,20 @@
 import React, { useState } from "react";
 import Form from "./Form";
-
+import { useSelector } from "react-redux";
 function Setting() {
+  const Profile = useSelector((state) => state.User);
   const [edit, setedit] = useState(false);
   return (
     <div className="setting">
-      {edit ? <Form /> : <div className="profile">hhhh</div>}
+      {edit ? (
+        <Form />
+      ) : (
+        <div className="profile">
+          <div className="imgbx">
+            <img alt="profile pic" scr={Profile.image && Profile.image} />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
