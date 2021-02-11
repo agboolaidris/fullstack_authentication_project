7e6f45f26e688857b2bbf28ec46524f7c3169c1b
idris agboola <agboolaisholaidreez@gmail.com>
2021-02-11 07:20:51 +0100

    update files

--- client/src/Component/Dashboard/userSetting/Setting.jsx
+++ client/src/Component/Dashboard/userSetting/Setting.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import Form from "./Form";
+import Img from "../../../img/idris.png";
 import { useSelector } from "react-redux";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearchLocation, faMap } from "@fortawesome/free-solid-svg-icons";
 function Setting() {
   const Profile = useSelector((state) => state.User);
   const [edit, setedit] = useState(false);
@@ -11,11 +14,16 @@ function Setting() {
       ) : (
         <div className="profile">
           <div className="imgbx">
-            <img alt="profile pic" scr={Profile.image && Profile.image} />
+            <img alt="profile pic" src={Img} />
           </div>
-          <div>
-            <p>Agboola idris</p>
-            <p>Software developer</p>
+          <div className="div">
+            <p className="name">Agboola idris</p>
+            <p className="email">agboolaisholaidreez@gmail.com</p>
+            <p className="job">Software developer</p>
+            <p className="city">
+              <FontAwesomeIcon icon={faMap} />
+              &nbsp; Lagos,Nigeria
+            </p>
           </div>
         </div>
       )}
