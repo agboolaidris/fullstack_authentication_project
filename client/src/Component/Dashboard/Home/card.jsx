e57362f28450d1f051422c8ad1b5a07c0e86b1c2
idris agboola <agboolaisholaidreez@gmail.com>
2021-02-24 15:39:06 +0100

    update

--- client/src/Component/Dashboard/Home/card.jsx
+++ client/src/Component/Dashboard/Home/card.jsx
@@ -11,32 +11,39 @@ function Card({ blog }) {
   //confirm if user have save the blog
   const userID = useSelector((state) => state.User._id);
   const confirmSave = blog.favourite.find((e) => e === userID);
-
   return (
     <>
-      <Link to={`/blog/${blog._id}`}>
-        <h2 className="title">{blog.title}</h2>
-        <div className="img-container">
-          <img src={blog.image} />
-        </div>
+      <Link to={`/blog/${blog._id}`} className="imgbx">
+        <img src={blog.image} />
       </Link>
       <div className="div">
+        <div className="tag">
+          <span>{blog.category.toUpperCase()}</span>
+          <p className="line"></p>
+        </div>
+        <h3>{blog.title}</h3>
+        <p className="article">
+          {blog.body.slice(0, 150)} &nbsp;
+          <Link to={`/blog/${blog._id}`}>Read more</Link>
+        </p>
+        <p className="lines"></p>
         <div className="icon">
           <button
             onClick={() => Dispatch(LikeBlog(blog._id))}
-            className={blog.like.length > 0 && "color"}
+            className={blog.like.length > 0 ? "color like" : "like"}
           >
             <FontAwesomeIcon icon={faThumbsUp} />
-            &nbsp;{blog.like.length}
+            <br />
+            {blog.like.length}
           </button>
           <button
             onClick={() => Dispatch(SaveBlog(blog._id))}
-            className={confirmSave && "color"}
+            className={confirmSave ? "color save" : "save"}
           >
-            <FontAwesomeIcon icon={faSave} /> &nbsp; save
+            <FontAwesomeIcon icon={faSave} /> &nbsp;
+            <br /> save
           </button>
         </div>
-        <span className="line"></span>
       </div>
     </>
   );
